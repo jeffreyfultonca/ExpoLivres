71ca3f6dd829cbd6d7190d7e45390fd5443237ce
Jeffrey Fulton <jeffrey.fulton@me.com>
2017-11-21 23:16:49 -0600

    Add UI Testing target with two screenshot tests.

---
+++ ExpoLivresUITests/ExpoLivresUITests.swift
@@ -0,0 +1,57 @@
+import XCTest
+
+class ExpoLivresUITests: XCTestCase {
+    
+    // MARK: - Stored Properties
+    
+    private var app: XCUIApplication!
+    private let persistenceService = PersistenceService.sharedInstance
+    
+    // MARK: - Setup & Tear-down
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        app = XCUIApplication()
+        app.launch()
+      
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        persistenceService.userOrganization = "UITesting"
+        persistenceService.userName = "UITesting"
+        persistenceService.userEmail = "ui@testing.com"
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    // MARK: - Tests
+    
+    func testCaptureLandingScreen() {
+        let screenshot = app.windows.firstMatch.screenshot()
+        let attachment = XCTAttachment(screenshot: screenshot)
+        attachment.lifetime = .keepAlways
+        attachment.name = "Landing Screen"
+        add(attachment)
+    }
+    
+    func testCaptureListScreen() {
+        app.buttons["Get Started"].tap()
+        
+        let _ = app.tables/*@START_MENU_TOKEN@*/.staticTexts["No items in list"]/*[[".cells.staticTexts[\"No items in list\"]",".staticTexts[\"No items in list\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.waitForExistence(timeout: 3)
+        
+        let screenshot = app.windows.firstMatch.screenshot()
+        let attachment = XCTAttachment(screenshot: screenshot)
+        attachment.lifetime = .keepAlways
+        attachment.name = "Empty List"
+        add(attachment)
+    }
+}
